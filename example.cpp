f6fca4a22e8e0d2692cb4c63c37869f48728fa4d
Whale <15207134228@163.com>
2022-03-18 17:13:09 +0800

    add lambda functions

--- example.cpp
+++ example.cpp
@@ -48,5 +48,11 @@ PYBIND11_MODULE(example, m)
     py::class_<Pet>(m, "Pet")
         .def(py::init<const std::string &>())
         .def("setName",&Pet::setName)
-        .def("getName",&Pet::getName);
+        .def("getName",&Pet::getName)
+        .def("__repr__",
+            [](const Pet &a)
+            {
+                return "example.Pet named '"+a.name+"'>";
+            }
+            );
 }
